eb9d92a818015e8052a70151db4e098d5b5f27f1
Dat <dev.tiendat2209@gmail.com>
2022-11-02 22:53:45 +0700

    javacript_modal_complete

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-let buyCards=document.querySelectorAll(".btn.btn-primary.btn-modal")
-let count=0;
+let btnModals=document.querySelectorAll(".btn-modal")
+let title=document.getElementsByClassName("card-title")
+let cost=document.getElementsByClassName("card-text")
+let reduceCost=document.getElementsByClassName("reduce-cost")
 let temp=0;
-for (const buyCard of buyCards){
-  buyCard.addEventListener('click',function(){
-    temp=1;
-    count++;
-    console.log("dat");
-    console.log(count);
-    })
+let i=0;
+for (let i=0;i<btnModals.length;i++){
+  btnModals[i].addEventListener('click',function(){
+    var string=title[2*i].innerText;
+    var type=string.slice(0,string.search(" "))
+    var modalText='Mệnh giá: '+cost[i].innerText+'<br/>'+'Loại thẻ: '+type+'<br/>'+'Giá: '+reduceCost[i].innerText;
+    document.getElementById("modal-text").innerHTML=modalText;
+    
+  })
 }
