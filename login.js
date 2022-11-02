3741fc5a96a3445981b434ec92adc24ea1196602
Dat <dev.tiendat2209@gmail.com>
2022-11-02 11:12:08 +0700

    javacript1

---
+++ login.js
@@ -0,0 +1,49 @@
+// <!-- Đức Phật nơi đây xin phù hộ code con chạy không Bug. Nam mô A Di Đà Phật.
+//                            _
+//                         _ooOoo_
+//                        o8888888o
+//                        88" . "88
+//                        (| -_- |)
+//                        O\  =  /O
+//                     ____/`---'\____
+//                   .'  \\|     |//  `.
+//                  /  \\|||  :  |||//  \
+//                 /  _||||| -:- |||||_  \
+//                 |   | \\\  -  /'| |   |
+//                 | \_|  `\`---'//  |_/ |
+//                 \  .-\__ `-. -'__/-.  /
+//               ___`. .'  /--.--\  `. .'___
+//            ."" '<  `.___\_<|>_/___.' _> \"".
+//           | | :  `- \`. ;`. _/; .'/ /  .' ; |
+//           \  \ `-.   \_\_`. _.'_/_/  -' _.' /
+// ===========`-.`___`-.__\ \___  /__.-'_.'_.-'================
+//                         `=--=-'         ＜（＾－＾）＞ -->
+let btnLogin = document.querySelector('#js-login-button');
+let username = document.getElementById('form3Example3');
+let password = document.getElementById('form3Example4');
+
+btnLogin.addEventListener('click', (e) => {
+  e.preventDefault();
+  let user = {
+    username: username.value,
+    password: password.value,
+  };
+  console.log(user);
+  let json = JSON.stringify(user);
+  if (!username.value || !password.value) {
+    alert("vui long nhap day du thong tin");
+  }
+  else{
+    if (username.value=="admin"&&password.value=="123"){
+      alert("Xin chao admin");
+      window.location.href = "admin.html";
+    }
+    else
+      if (localStorage.getItem(username.value) == json) {
+        alert("Dang nhap thanh cong");
+        window.location.href = "index.html";
+      } else {
+        alert("Dang nhap that bai");
+      }
+    }
+});
---
+++ main.js
@@ -0,0 +1,25 @@
+let username = document.getElementById("nameinput");
+let email = document.getElementById("form3Example3");
+let password = document.getElementById("form3Example4");
+let repassword = document.getElementById("form3Example4d");
+let btnLogin = document.querySelector(".js-login-button");
+
+btnLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  let user = {
+    username: username.value,
+    email: email.value,
+    password: password.value,
+    repassword: password.value,
+  };
+  let json = JSON.stringify(user);
+  if (!username.value || !email.value || !password.value || !repassword.value) {
+    alert("Vui long nhap day du thong tin");
+  } else {
+    if (!password.value != !repassword.value)
+      alert("Mat khau khong khop");
+    localStorage.setItem(username.value, json);
+    alert("Dang ky thanh cong");
+    window.location.href="index.html";
+  }
+});
