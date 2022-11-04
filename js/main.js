a01cbd39fb83ba0f1fe676fefa0f0298ecf607c0
Dat <dev.tiendat2209@gmail.com>
2022-11-04 20:03:13 +0700

    select1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 // modal
 let btnModals=document.querySelectorAll(".btn-modal")
 let title=document.getElementsByClassName("card-title")
@@ -12,3 +11,41 @@ for (let i=0;i<btnModals.length;i++){
     document.getElementById("modal-text").innerHTML=modalText;   
   })
 }
+let user = {
+  username: "",
+  password: "",
+};
+let json = JSON.stringify(user);
+console.log(user.username.value);
+// login
+let headerLogin=document.querySelector('.header-login')
+let headerUser=document.querySelector('.header-user')
+let btnLogin = document.querySelector('#js-login-button');
+let username = document.getElementById('form3Example3');
+let password = document.getElementById('form3Example4');
+btnLogin.addEventListener('click', (e) => {
+  e.preventDefault();
+  let user = {
+    username: username.value,
+    password: password.value,
+  };
+  console.log(user);
+  let json = JSON.stringify(user);
+  let json1=JSON.stringify(temp)
+  if (!username.value || !password.value) {
+    alert("vui long nhap day du thong tin");
+  }
+  else{
+    if (username.value=="admin"&&password.value=="123"){
+      alert("Xin chao admin");
+      window.location.href = "admin.html"
+    }
+    else
+      if (localStorage.getItem(username.value) == json) {
+        alert("Dang nhap thanh cong");
+        window.location.href = "index.html";
+      } else {
+        alert("Dang nhap that bai");
+      }
+    }
+});
